e733f785245d367c5d38a31e325a69ca3b3beb82
Ferran Espasa <fespasa@me.com>
2018-03-20 13:46:34 +0100

    Renewed Code Meets Expecs Done
    Recoded all and Meets Expectations tasks done and working.

--- pagination-filter-v2/js/main.js
+++ pagination-filter-v2/js/main.js
@@ -1,29 +1,47 @@
 const students = document.querySelectorAll('.student-item');
-const pagination = document.querySelector('.pagination');
+const page = document.querySelector('.page');
+let activeLink = 1;
 
+showPage(activeLink, students);
+if(students.length > 10){
+	let paginationDiv = document.createElement("DIV");
+	paginationDiv.setAttribute("class", "pagination");
+	page.appendChild(paginationDiv);
+	appendPageLinks(students);
+}
 
-console.log(students);
-
-for(let i = 0; i < students.length; i++) {
-	if(i >= 10){
-		students[i].style.display = "none";
+function showPage(activeLink, students) {
+	for(let i = 0; i < students.length; i++){
+		if(i < activeLink * 10 && i >= activeLink * 10 - 10){
+			students[i].style.display = "block";
+		} else {
+			students[i].style.display = "none";
+		}
 	}
 }
 
-if(students.length > 10){
-	const n = Math.floor(students.length / 10) + 1;
-	console.log("n is " + n);
-	pagination.innerHTML = "<ul></ul>";
-	for(let i = 1; i <= n; i++) {
-		let li = document.createElement("li");
-		if(i === 1){
-			li.innerHTML = "<a class='active' href='#'>" + i + "</a>";
-			pagination.appendChild(li);
-		} else {
-			li.innerHTML = "<a href='#'>" + i + "</a>";
-			pagination.appendChild(li);
-		}
+function appendPageLinks(students) {
+	const pagination = document.querySelector('.pagination');
+	let numPages;
+	let ulNode = pagination.appendChild(document.createElement("UL"));
+	if(students.length % 10 !== 0){
+		numPages = Math.floor(students.length / 10) + 1;
+	} else {
+		numPages = students.length / 10;
 	}
-} else {
-	pagination.style.display = "none";
+	for(let i = 1; i <= numPages; i++){
+		let liNode = document.createElement("LI");
+		let liItem = ulNode.appendChild(liNode);
+		if( i === activeLink ) {
+			liItem.innerHTML = '<a href="#" class="active">' + i + '</a>';
+		} else {
+			liItem.innerHTML = '<a href="#">' + i + '</a>';
+		}
+		liItem.addEventListener('click', () => {
+			activeLink = i;
+			showPage(activeLink, students);
+			pagination.removeChild(pagination.childNodes[0]);
+			appendPageLinks(students);
+		})
+	}
 }
