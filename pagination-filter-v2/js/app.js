5a8bb580e4c6bbcadea383c00b42cc48cb43f838
Ferran Espasa <fespasa@me.com>
2018-03-22 16:47:44 +0100

    Change to jQuery
    We adapted our code to jQuery.

---
+++ pagination-filter-v2/js/app.js
@@ -0,0 +1,47 @@
+const $students = $('.student-item');
+
+const maxListStudents = 10;
+let pageNum = 3;
+
+
+showPage(pageNum, $students);
+appendPageLinks($students);
+
+function showPage(pageNum, studentList) {
+	// loop through all students
+	studentList.each(function(){
+		// hide each student
+		$(this).hide();
+		if($(this).index() >= (pageNum - 1) * maxListStudents && $(this).index() < pageNum * maxListStudents) {
+			// display the student if it has to be displayed
+			$(this).show();
+		}
+	})
+}
+
+function appendPageLinks(studentList) {
+	let pages = Math.floor(studentList.length / maxListStudents) + 1;
+	if($('.pagination') !== undefined) {
+		$('.pagination').hide();
+	}
+	const $linksList = $('<div class="pagination"><ul></ul></div>');
+	$('.page').append($linksList);
+	for(let i = 1; i <= pages; i++) {
+		console.log("Inside Bucle at: " + i);
+		if(i === pageNum){
+			console.log("i is: " + i + "compared to: " + pageNum);
+			$('.pagination ul').append($('<li><a href="#" class="active">'+ i +'</a></li>)'));;
+		} else {
+			$('.pagination ul').append($('<li><a href="#">'+ i +'</a></li>)'));
+		}
+	}
+	$('.pagination a').click(function(){
+		console.log($(this));
+		let linkClicked = parseInt($(this)[0].textContent);
+		console.log("I clicked the: "+ linkClicked + " button.");
+		pageNum = linkClicked;
+		console.log(pageNum);
+		showPage(pageNum, studentList); 
+		appendPageLinks(studentList);
+	});
+}
